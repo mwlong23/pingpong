9f00e255d5272c56047478168cd57b802be1df77
Mitchell Long and Snow Vilay <snow@>
2017-09-05 11:35:15 -0700

    check if 3 and 5 needs to be replaced with ping or pong

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,10 +1,40 @@
+#
+#   public def ping_pong()
+#     array = []
+#     x = 0
+#     while (x < 4)
+#       x = x + 1
+#       if x.%(3).eql?(0)
+#         # array.push("ping")
+#         # # binding.pry
+#       # else
+#       array.push(x)
+#       end
+#     end
+#   array.each() do |currentElement|
+#     if currentElement.%(3).eql?(0)
+#       array[currentElement].push(("ping"))
+#     end
+#   end
+# end
 
-  public def ping_pong()
-    array = []
-    x = 0
-    while (x < 4)
-      x = x + 1
+
+public def ping_pong()
+  # array = Array.new
+  array=[]
+  x = 0
+  while (x < 5)
+    x = x + 1
+    if !(x.eql?(3) || x.eql?(5))
       array.push(x)
+    elsif (x.%(5).eql?(0))
+      array.push("pong")
+    else
+      array.push("ping")
     end
+  end
   array
 end
+
+
+# || !(x.%(5).eql?(0))
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -1,8 +1,12 @@
 require('rspec')
 require('ping_pong')
+require('pry')
 
 describe('ping_pong') do
   it("form an array that counts to the inputted number") do
-    expect((4).ping_pong).to(eq([1,2,3,4,5]))
+    expect((5).ping_pong).to(eq([1,2,"ping",4,"pong"]))
+  end
+  it("check if current element of the array is divisible by 3") do
+    expect((5).ping_pong).to(eq([1,2,"ping",4,"pong"]))
   end
 end
